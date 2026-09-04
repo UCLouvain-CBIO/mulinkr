6b2bd631242810eabd40a1c1ff19148b7eb251bf
Laurent Gatto <lgatto@protonmail.ch>
2026-09-04 21:38:46 +0200

    add hasScpModel()

--- R/scpmodel.R
+++ R/scpmodel.R
@@ -1,18 +1,17 @@
-scpModelFitListToList <- function(fit) {
-    stopifnot(inherits(fit, "ScpModelFit"))
-    list(n = fit@n,
-         p = fit@p,
-         coefficients = fit@coefficients,
-         residuals = fit@residuals,
-         effects = as.list(fit@effects),
-         df = fit@df,
-         var = fit@var,
-         uvcov = fit@uvcov,
-         levels = as.list(fit@levels))
-}
-
 scpModelToList <- function(mod) {
     stopifnot(inherits(mod, "ScpModel"))
+    scpModelFitListToList <- function(fit) {
+        stopifnot(inherits(fit, "ScpModelFit"))
+        list(n = fit@n,
+             p = fit@p,
+             coefficients = fit@coefficients,
+             residuals = fit@residuals,
+             effects = as.list(fit@effects),
+             df = fit@df,
+             var = fit@var,
+             uvcov = fit@uvcov,
+             levels = as.list(fit@levels))
+    }
     list(
         scpModelFormula = paste(as.character(mod@scpModelFormula), collapse = " "),
         scpModelInputIndex = mod@scpModelInputIndex,
@@ -21,22 +20,29 @@ scpModelToList <- function(mod) {
                                  scpModelFitListToList))
 }
 
-listToScpModelFit <- function(x)
-    new("ScpModelFit",
-        n = x$n,
-        p = x$p,
-        coefficients = x$coefficients,
-        residuals = x$residuals,
-        effects = List(x$effects),
-        df = x$df,
-        var = x$var,
-        uvcov = x$uvcov,
-        levels = List(x$levels))
-
-
-listToScpModel <- function(x)
+listToScpModel <- function(x) {
+    listToScpModelFit <- function(x)
+        new("ScpModelFit",
+            n = x$n,
+            p = x$p,
+            coefficients = x$coefficients,
+            residuals = x$residuals,
+            effects = List(x$effects),
+            df = x$df,
+            var = x$var,
+            uvcov = x$uvcov,
+            levels = List(x$levels))
     new("ScpModel",
         scpModelFormula = as.formula(x$scpModelFormula),
         scpModelInputIndex = x$scpModelInputIndex,
         scpModelFilterThreshold = x$scpModelFilterThreshold,
         scpModelFitList = List(lapply(ll$scpModelFitList, listToScpModelFit)))
+}
+
+hasScpModel <- function(object) {
+    stopifnot(inherits(object, "SummarizedExperiment"))
+    if (!length(metadata(object)))
+        return(FALSE)
+    any(sapply(metadata(object),
+               function(x) inherits(x, "ScpModel")))
+}
--- tests/testthat/test-scpModel.R
+++ tests/testthat/test-scpModel.R
@@ -8,3 +8,11 @@ test_that("Convert scpModel to/from list", {
     expect_true(validObject(mod <- listToScpModel(ll)))
     expect_equivalent(mod0, mod)
 })
+
+test_that("hasScpModel works", {
+    expect_true(hasScpModel(leduc_minimal))
+    metadata(leduc_minimal) <- list()
+    expect_false(hasScpModel(leduc_minimal))
+    metadata(leduc_minimal) <- list(1, letters, ls)
+    expect_false(hasScpModel(leduc_minimal))
+})
